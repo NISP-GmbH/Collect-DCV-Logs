ba89eaf20ed9dd0fa2e5c32b78a9446220bc53fa
Francisco Panis Kaseker <franciscopk@ni-sp.com>
2024-06-10 10:38:24 -0300

    typo

--- code/library.sh
+++ code/library.sh
@@ -162,7 +162,7 @@ getXorgData()
 
     sudo X -configure > ${target_dir}/xorg.conf.configure.stdout 2> ${target_dir}xorg.conf.configure.stderr
     x_display=$(sudo ps aux | egrep '(X|Xorg|Xwayland)' | awk '{for (i=1; i<=NF; i++) if ($i ~ /^:[0-9]+$/) print $i}')
-    if [[ "${x_display}x" == "x"x ]]
+    if [[ "${x_display}x" == "x" ]]
     then
         echo "not possible to execute xrandr: display not found" > ${target_dir}/xrandr_can_not_be_executed
     else
