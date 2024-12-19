1e48708ab3cf77150f72b58fe14e5c96bdef6ed3
Francisco Panis Kaseker <franciscopk@ni-sp.com>
2024-12-19 08:52:40 +0100

    increase journalctl lines to 30k

--- code/library.sh
+++ code/library.sh
@@ -383,7 +383,7 @@ getDcvDataAfterReboot()
             sudo cp -r /var/log/dcv ${target_dir} > /dev/null 2>&1
         else
             echo "not found" > $target_dir/var_log_dcv_not_found
-            sudo journalctl -n 15000 > ${target_dir}/journal_last_15000_lines.log
+            sudo journalctl -n 30000 > ${target_dir}/journal_last_30000_lines.log
             sudo journalctl --no-page | grep -i selinux > ${target_dir}/selinux_log_from_journal
             sudo journalctl --no-page | grep -i apparmor > ${target_dir}/apparmor_log_from_journal
         fi 
@@ -651,7 +651,7 @@ getOsData()
     fi
 
     target_dir="${temp_dir}/journal_log"
-    sudo journalctl -n 15000 > ${target_dir}/journal_last_15000_lines.log 2>&1
+    sudo journalctl -n 30000 > ${target_dir}/journal_last_30000_lines.log 2>&1
     sudo journalctl --no-page | grep -i selinux > ${target_dir}/selinux_log_from_journal 2>&1
     sudo journalctl --no-page | grep -i apparmor > ${target_dir}/apparmor_log_from_journal 2>&1
 
